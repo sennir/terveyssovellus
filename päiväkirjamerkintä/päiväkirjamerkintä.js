ae0a0681f0da4d13fd1d9c52a533f8533cd38ae0
vernerss1 <verneri.solonen@metropolia.fi>
2024-04-24 16:07:55 +0300

    Aktiivisen mielialan tyylittely

--- päiväkirjamerkintä/päiväkirjamerkintä.js
+++ päiväkirjamerkintä/päiväkirjamerkintä.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
     $('.mood-option').click(function() {
         const selectedMood = $(this).data('value');
         $('#selectedMood').val(selectedMood);
+        $('.mood-option').removeClass('active'); // Remove active class from all mood options
+        $(this).addClass('active'); // Add active class to the selected mood option
+        console.log($('.mood-option.active').data('value'));
     });
 });
+
